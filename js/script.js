bcfb1136e3a44bc3af0d9f0f6e0bf45584f983e5
Setyo Dwiki Prastomo <102672040+setyodwikiprastomo@users.noreply.github.com>
2023-05-08 22:16:44 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
